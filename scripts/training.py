0f1ee4dc7e0cfc6657c54fc7c826cce97fe43548
Giorgio Mondauto <giorgiomondauto@Giorgios-MacBook-Pro.local>
2020-04-22 10:52:40 +0100

    cleaning repo

--- scripts/training.py
+++
@@ -1,8 +0,0 @@
-from sklearn.naive_bayes import MultinomialNB
-
-def model_training(training_data, y_train):
-    ''' train the model '''
-    naive_bayes = MultinomialNB()
-    naive_bayes.fit(training_data, y_train)
-    return naive_bayes
-
