3507733238bdffc71f212c468d907a25aa625b0f
Giorgio Mondauto <giorgiomondauto@Giorgios-MacBook-Pro.local>
2020-05-04 10:14:31 +0100

    saved

--- run_api.py
+++ run_api.py
@@ -20,7 +20,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer
-
+nltk.download('punkt')
 def get_key(value, dictionary):
     for k,v in dictionary.items():
         if value == v:
